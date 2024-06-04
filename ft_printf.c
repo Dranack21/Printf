c893c3671a23a493632866e1c5316b95a54ab6a9
Hugo <hugoabouda@gmail.com>
2024-06-05 00:47:18 +0200

    j'ai faim

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 12:38:43 by habouda           #+#    #+#             */
-/*   Updated: 2024/06/04 23:44:13 by habouda          ###   ########.fr       */
+/*   Updated: 2024/06/05 00:46:34 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ int	check_after(const char *str, va_list args)
 		ft_putchar(va_arg(args, int));
 		i++;
 	}
-	if (str[i] && (str[1] == 'x'))
+	if (str[1] && (str[1] == 'x'))
 		i = i + ft_hexa_lower(va_arg(args, int));
-	if (str[i] && (str[1] == 'X'))
+	if (str[1] && (str[1] == 'X'))
 		i = i + ft_hexa_upper(va_arg(args, int));
-	if ((str[i] && str[1] == 'd') || str[0] == 'i' || str[0] == 'u')
+	if ((str[1] && str[1] == 'd') || str[1] == 'i' || str[1] == 'u')
 		i = i + ft_putnbr(va_arg(args, int));
-	if (str[i] && str[1] == 's')
+	if (str[1] && str[1] == 's')
 		i = i + ft_putstr(va_arg(args, char *));
-	if (str[i] && str[1] == '%')
+	if (str[1] && str[1] == '%')
 	{
 		i++;
 		ft_putchar('%');
@@ -44,28 +44,46 @@ int	ft_printf(const char* str, ...)
 {
 	va_list args;
 	int		i;
-	int		k;
+	int		length;
+	int 	wrote;
 
 	i = 0;
+	wrote = 0;
 	va_start(args, str);
 	while (str[i] != '\0')
 	{
-		if (str[i] != '\0' && str[i] == '%')
+		if (str[i] == '%')
 		{
-			k = check_after(&str[i], args);
+			length = check_after(&str[i], args);
+			wrote = wrote + length;
 			i++;
 		}
 		else
+		{
 			ft_putchar(str[i]);
+			wrote++;
+		}
 		i++;
 	}
 	va_end(args);
-	return (k);
+	return (wrote);
 }
-/*#include <stdio.h>
+#include <stdio.h>
 
-int	main()
-{
-	ft_printf("%s %x \n" ,"efgh", 56);
-	printf("%s %x \n" ,"efgh", 56);
-}*/
+
+int main() {
+    int number = 6719;
+
+    // Using the standard printf
+    printf("Standard printf with %%x: %x\n", number);
+
+    // Using the custom ft_printf
+    ft_printf("Custom ft_printf with %%x: %x\n", number);
+
+    // Testing with another value
+    number = 65518;
+    printf("Standard printf with %%x: %x\n", number);
+    ft_printf("Custom ft_printf with %%x: %x\n", number);
+
+    return 0;
+}
